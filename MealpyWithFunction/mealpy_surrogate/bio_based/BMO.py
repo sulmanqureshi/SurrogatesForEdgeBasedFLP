62895001b1939a0562c3e1376ae64aacbb621b4b
Zeus <32224662+sulmanqureshi@users.noreply.github.com>
2024-01-24 14:47:22 +0100

    Add files via upload

---
+++ MealpyWithFunction/mealpy_surrogate/bio_based/BMO.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 17:31, 21/05/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+# https://ieeexplore.ieee.org/document/8441097
---
+++ MealpyWithFunction/mealpy_surrogate/bio_based/SOA.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 17:21, 21/05/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+# https://www.sciencedirect.com/science/article/abs/pii/S0950705118305768
+
