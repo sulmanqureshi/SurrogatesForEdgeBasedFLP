fd55d6532ee8f8dbd9e51e9c53dae78511e0e7a6
Zeus <32224662+sulmanqureshi@users.noreply.github.com>
2024-01-24 14:46:36 +0100

    Add files via upload

---
+++ MealpyWithFunction/mealpy_surrogate/swarm_based/OPA.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 17:45, 21/05/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+# https://www.sciencedirect.com/science/article/pii/S0957417421013725?casa_token=3SWmELleVaAAAAAA:HkfblcJ-FWaGWyXDiRLyBSfm9QnUqMTBLIucZO01lfz4kS1P2Xf9gbdw1B-PvIVRrsp4xKSY4g
---
+++ MealpyWithFunction/mealpy_surrogate/swarm_based/WSO.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 17:43, 21/05/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+# https://www.researchgate.net/publication/358788372_White_Shark_Optimizer_A_novel_bio-inspired_meta-heuristic_algorithm_for_global_optimization_problems
