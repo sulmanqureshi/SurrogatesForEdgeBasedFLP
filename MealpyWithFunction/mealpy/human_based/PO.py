0032949cb6eb9f8a11d2c1f1bfeb3fc9350b9096
Zeus <32224662+sulmanqureshi@users.noreply.github.com>
2024-01-24 14:38:43 +0100

    Add files via upload

---
+++ MealpyWithFunction/mealpy/human_based/PO.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 17:18, 21/05/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+# https://www.sciencedirect.com/science/article/abs/pii/S0950705120301350?casa_token=q_VL1tEmzSIAAAAA:2Ej1HYMPzTHCd6LxYop9cQmuNevHCfYviol8RYq964yCn_SkhKyIViZu20gOK3AgAaYsztcMdc8
---
+++ MealpyWithFunction/mealpy/human_based/WarSO.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 17:41, 21/05/2022 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+# https://www.researchgate.net/publication/358806739_War_Strategy_Optimization_Algorithm_A_New_Effective_Metaheuristic_Algorithm_for_Global_Optimization
